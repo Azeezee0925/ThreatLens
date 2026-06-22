c70c3d38e82ac482915ee9a99d14f1bc2061af31
Azeeza <maalinazeeza@gmail.com>
2026-06-22 23:28:40 +0530

    made changes and edited .gitignore, added api_key to .env

--- backend/app/api/investigation.py
+++ backend/app/api/investigation.py
@@ -1,13 +1,15 @@
 from fastapi import APIRouter
 from app.schemas.investigation import InvestigationRequest
+from app.services.threat_service import investigate_ioc
 
 router = APIRouter()
 
 
 @router.post("/investigate")
 def investigate(request: InvestigationRequest):
-    return {
-        "message": "Investigation request received",
-        "ioc": request.ioc,
-        "ioc_type": request.ioc_type
-    }
+    result = investigate_ioc(
+        request.ioc,
+        request.ioc_type
+    )
+
+    return result
---
+++ backend/app/services/threat_service.py
@@ -0,0 +1,9 @@
+def investigate_ioc(ioc: str, ioc_type: str):
+
+    return {
+        "ioc": ioc,
+        "ioc_type": ioc_type,
+        "status": "Safe",
+        "threat_score": 10,
+        "source": "Mock Threat Service"
+    }
